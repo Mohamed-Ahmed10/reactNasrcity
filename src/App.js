06a655e29331e4dc91507c6f620f195fbc8654dc
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2022-04-27 22:51:59 +0200

    First lecture

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Nav from "./components/nav";
 
 function App() {
     var myName = "Mohamed";
+    var black = "#000";
     return (
         <div className="App">
             <Nav />
-            <input type="text" />
+            <input
+                type="text"
+                style={{backgroundColor: {black}, color: "#FFF"}}
+            />
             Hello
             {myName}
             <button onClick={test}>OK</button>
